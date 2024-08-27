eb0e059afd3281cdf3bd28b69033c5a8ba06062c
First Snow <show2633@naver.com>
2024-08-27 09:14:34 +0900

    [Feat]: StoryBoard 제거

---
+++ PlantDiary/PlantDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantDiary
+//
+//  Created by Hyunwoo Shin on 8/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+#Preview {
+    ViewController()
+}
