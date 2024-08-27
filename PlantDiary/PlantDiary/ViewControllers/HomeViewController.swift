da2027f685d98b7bcd4e5226034515c04d923e5e
First Snow <show2633@naver.com>
2024-08-27 09:57:12 +0900

    [Add]: Base ViewControllers 작성, NavigationController 추가
    1. SceneDelegate
     - 각 ViewController NavigationController 연결
     - TabBarController 설정
    2. HomeViewController
     - 파일 작성
    3. DiaryViewController
     - 파일 작성
    4. PlantsCareViewController
     - 파일 작성
    5. SettingViewController
     - 파일 작성

--- PlantDiary/PlantDiary/ViewControllers/HomeViewController.swift
+++ PlantDiary/PlantDiary/ViewControllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  PlantDiary
 //
 //  Created by Hyunwoo Shin on 8/27/24.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
 #Preview {
-    ViewController()
+    HomeViewController()
 }
